abd0281488bae67bfb31d4c67c92bb6a5e5e1119
Scott Rubin <srubin@broadway.com>
2013-07-08 16:58:00 -0400

    Created setup script and updated README

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+rm -f ~/.vimrc
+ln -s ~/.vim/vimrc ~/.vimrc
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+vim +BundleInstall +qall
+cd ~/.vim/bundle/YouCompleteMe && ./install.sh
